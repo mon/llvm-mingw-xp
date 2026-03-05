92b344f70dcf60408fffd7076831e04767afac1e
Will Toohey <will@mon.im>
2026-03-05 19:10:13 +1000

    Dockerfile

---
+++ toolchain-files/cmake/i686-mingw32-clang.cmake
@@ -0,0 +1,13 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_C_COMPILER   i686-w64-mingw32-clang)
+set(CMAKE_CXX_COMPILER i686-w64-mingw32-clang++)
+set(CMAKE_RC_COMPILER  i686-w64-mingw32-windres)
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static")
+
+# adjust the default behavior of the FIND_XXX() commands:
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
